0c2a78bc248ea39acd3229eb4d9b12708d593e64
Kazumasa Utashiro <kaz@utashiro.com>
2025-05-26 14:06:33 +0900

    simplify parameter handling

--- getoptlong.sh
+++ getoptlong.sh
@@ -15,39 +15,39 @@ tgl_conf()  { tgl_opts \&"$1" "${@:2}" ; }
 tgl_type()  { tgl_opts \:"$1" "${@:2}" ; }
 tgl_alias() { tgl_opts \="$1" "${@:2}" ; }
 tgl_hook()  { tgl_opts \!"$1" "${@:2}" ; }
-tgl_debug() { [[ ${_opts["&DEBUG"]} ]] || return 0; tgl_warn DEBUG: "${@}" ; }
+tgl_valid() { [[ -v _opts[:$1] ]] }
+tgl_debug() { [[ ${_opts["&DEBUG"]:-} ]] || return 0; tgl_warn DEBUG: "${@}" ; }
 tgl_dump() {
     local declare="$(declare -p TGL_OPTS)"
     if [[ "$declare" =~ \"(.+)\" ]] ; then
-	declare -p ${BASH_REMATCH[1]} | grep -o -E '\[[^]]*\]="[^"]*"' | sort
+	declare -p ${BASH_REMATCH[1]} | grep -oE '\[[^]]*\]="[^"]*"' | sort
     fi
 }
 tgl_setup() {
+    (( $# == 0 )) && { echo 'local TGL_OPTS OPTIND=1' ; return ; }
     declare -A TGL_CONFIG=(
-	[EXIT_ON_ERROR]=yes [SILENT]= [SAVETO]=
-	[TRUE]=yes [FALSE]= [DEBUG]=
-	[MARKS]=':=!&' [MK_TYPE]=':' [MK_ALIAS]='=' [MK_HOOK]='!' [MK_CONF]='&'
-	[TYPES]=':@+?' [TP_NEED]=':' [TP_ARRAY]='@' [TP_INCR]='+' [TP_MAY]='?'
+	[EXIT_ON_ERROR]=yes [SILENT]= [SAVETO]= [TRUE]=yes [FALSE]= [DEBUG]=
     )
-    if (( $# == 0 )) ; then
-	echo 'local TGL_OPTS OPTIND=1'
-    else
-	TGL_OPTS=$1
-	tgl_setup_ "$@"
-    fi
+    declare -n _opts=$1
+    for key in "${!TGL_CONFIG[@]}" ; do _opts[&$key]="${TGL_CONFIG[$key]}" ; done
+    TGL_OPTS=$1
+    tgl_redirect "${@:2}"
+}
+tgl_redirect() {
+    declare -n _opts=$TGL_OPTS
+    tgl_debug "${FUNCNAME[1]}(${@@Q})"
+    local MARKS=':=!&' MK_TYPE=':' MK_ALIAS='=' MK_HOOK='!' MK_CONF='&' \
+	  TYPES=':@+?' TP_ARGS=":@" TP_NEED=":" TP_MAY="?" TP_ARRAY="@" TP_INCR="+"
+    local TRUE=${_opts[${MK_CONF}TRUE]} FALSE=${_opts[${MK_CONF}FALSE]}
+    declare -n MATCH=BASH_REMATCH
+    "${FUNCNAME[1]}_" $TGL_OPTS "$@"
 }
 tgl_setup_() {
     declare -n _opts=$1; shift
-    # set default config parameters
-    for key in "${!TGL_CONFIG[@]}" ; do tgl_conf "$key" "${TGL_CONFIG[$key]}" ; done
-    local key marks="$(tgl_conf MARKS)" tp_array=$(tgl_conf TP_ARRAY)
-    local types=$(tgl_conf TYPES)
     for key in "${!_opts[@]}" ; do
-	[[ $key =~ ^[$marks] ]] && continue
-	if [[ $key =~ ^([-_ \|[:alnum:]]+)([$types]*)( *)$ ]] ; then
-	    local names=${BASH_REMATCH[1]}
-	    local type=${BASH_REMATCH[2]}
-	    local aliases alias
+	[[ $key =~ ^[$MARKS] ]] && continue
+	if [[ $key =~ ^([-_ \|[:alnum:]]+)([$TYPES]*)( *)$ ]] ; then
+	    local names=${MATCH[1]} type=${MATCH[2]} aliases alias
 	    IFS=' |' read -a aliases <<<$names
 	    local name=${aliases[0]}
 	    tgl_type "$name" "$type"
@@ -56,14 +56,13 @@ tgl_setup_() {
 		tgl_type  "$alias" "$type"
 	    done
 	    case $type in
-		$tp_array)
+		[$TP_ARRAY])
 		    declare -n array=$name
 		    [[ ${_opts[$key]} ]] && array=("${_opts[$key]}") || array=()
 		    _opts[$name]=$name
 		    ;;
-		*) 
-		    [[ $name != $key ]] && _opts[$name]=${_opts[$key]}
-		    ;;
+		[$TP_MAY]) ;;
+		*) [[ $name != $key ]] && _opts[$name]=${_opts[$key]} ;;
 	    esac
 	else
 	    tgl_warn "[$key] -- option description error"
@@ -73,11 +72,6 @@ tgl_setup_() {
     (( $# > 0 )) && tgl_configure "$@"
     return 0
 }
-tgl_redirect() {
-    declare -n _opts=$TGL_OPTS
-    tgl_debug "${FUNCNAME[1]}(${@@Q})"
-    "${FUNCNAME[1]}_" $TGL_OPTS "$@"
-}
 tgl_configure () { tgl_redirect "$@" ; }
 tgl_configure_() {
     declare -n _opts=$1; shift
@@ -85,28 +79,25 @@ tgl_configure_() {
 	[[ $param =~ ^[[:alnum:]] ]] || tgl_die "$param -- invalid config parameter"
 	local key val
 	if [[ $param =~ = ]] ; then
-	    key="&${param%%=*}"
+	    key="${MK_CONF}${param%%=*}"
 	    val="${param#*=}"
 	else
-	    key="&${param}"
-	    val="$(tgl_conf TRUE)"
+	    key="${MK_CONF}${param}"
+	    val="$TRUE"
 	fi
-	if [[ ${_opts[$key]+_} ]] ; then
-	    _opts[$key]="$val"
-	else
-	    tgl_die "$param -- invalid config parameter"
-	fi
+	[[ -v _opts[$key] ]] || tgl_die "$param -- invalid config parameter"
+	_opts[$key]="$val"
     done
     return 0
 }
 tgl_string () { tgl_redirect "$@" ; }
 tgl_string_() {
     declare -n _opts=$1; shift
-    local key string mark=$(tgl_conf MK_TYPE)
-    local tp_args="$(tgl_conf TP_NEED)$(tgl_conf TP_ARRAY)"
+    local key string
     for key in ${!_opts[@]} ; do
-	[[ $key =~ ^${mark}.$ ]] || continue
-	[[ ${_opts[$key]} =~ [$tp_args] ]] && string+="${key#:}:" || string+=${key#:}
+	[[ $key =~ ^${MK_TYPE}(.)$ ]] || continue
+	string+=${MATCH[1]}
+	[[ ${_opts[$key]} =~ [$TP_ARGS] ]] && string+=:
     done
     [[ $(tgl_conf SILENT) ]] && string=":$string"
     string+="-:"
@@ -118,8 +109,6 @@ tgl_getopts_() {
     declare -n _opts=$1; shift
     local opt="$1"; shift;
     local name val type
-    local tp_need="$(tgl_conf TP_NEED)" tp_may="$(tgl_conf TP_MAY)" tp_array="$(tgl_conf TP_ARRAY)" tp_incr="$(tgl_conf TP_INCR)"
-    local tp_args="${tp_need}${tp_array}"
     case $opt in
 	[:?])
 	    local hook=$(tgl_hook "$opt")
@@ -129,43 +118,41 @@ tgl_getopts_() {
 	    ;;
 	-)
 	    [[ $OPTARG =~ ^(no-?)?([-_[:alnum:]]+)(=(.*))? ]] || die "$OPTARG: unrecognized option"
-	    local no="${BASH_REMATCH[1]}" nm="${BASH_REMATCH[2]}" param="${BASH_REMATCH[3]}"; val="${BASH_REMATCH[4]}"
+	    local no="${MATCH[1]}" nm="${MATCH[2]}" param="${MATCH[3]}"; val="${MATCH[4]}"
 	    name=$(tgl_alias $nm) || name=$nm
 	    type=$(tgl_type $name)
-	    [[ -v _opts[$name] ]] || tgl_die "--$name: no such option"
+	    tgl_valid $name || tgl_die "no such option -- $name"
 	    if [[ $param ]] ; then
-		[[ $type =~ [${tp_args}${tp_may}] ]] || die "does not take an argument -- $name"
+		[[ $type =~ [${TP_ARGS}${TP_MAY}] ]] || die "does not take an argument -- $name"
 	    else
 		case $type in
-		    [$tp_may]) ;;
-		    [$tp_incr]) val=$(( _opts[$name] + 1 )) ;;
-		    [$tp_args])
+		    [$TP_MAY]) ;;
+		    [$TP_INCR]) val=$(( _opts[$name] + 1 )) ;;
+		    [$TP_ARGS])
 			(( OPTIND > $# )) && tgl_die "option requires an argument -- $name"
 			val=${@:$((OPTIND)):1}
 			(( OPTIND++ ))
 			;;
-		    *)
-			[[ $no ]] && val=$(tgl_conf FALSE) || val=$(tgl_conf TRUE)
-			;;
+		    *) [[ $no ]] && val="$FALSE" || val="$TRUE" ;;
 		esac
 	    fi
 	    ;;
 	*)
 	    name=$(tgl_alias $opt) || name=$opt
 	    case ${type:=$(tgl_type "$name")} in
-		[$tp_incr]) val=$(( _opts[$name] + 1 )) ;;
-		[$tp_args]) val="${OPTARG}" ;;
-		*)            val=$(tgl_conf TRUE) ;;
+		[$TP_MAY])  val="${OPTARG:-}" ;;
+		[$TP_INCR]) val=$(( _opts[$name] + 1 )) ;;
+		[$TP_ARGS]) val="${OPTARG}" ;;
+		*)          val="$TRUE" ;;
 	    esac
 	    ;;
     esac
     case $type in
-	[$tp_array])
+	[$TP_ARRAY])
 	    declare -n array=${_opts[@$name]:-$name}; array+=($val) ;;
 	*)
 	    _opts[$name]="$val" ;;
     esac
-    # callback
     local callback
     callback="$(tgl_hook $name)" && $callback "$val"
     return 0
@@ -191,8 +178,7 @@ tgl_getoptlong_() {
 	tgl_getopts "$tgl_OPT" "$@"
     done
     shift $(( OPTIND - 1 ))
-    [[ ${_opts[&DEBUG]} ]] && echo "DEBUG: ARGV=$@"
-    # save result
+    tgl_debug "ARGV=(${@@Q})"
     local array="$(tgl_conf SAVETO)"
     [[ $array ]] && { declare -n argv=$array ; argv=("$@") ; }
     return 0
