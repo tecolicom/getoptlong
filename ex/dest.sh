ea29b9b6b487f9a0f09505a424ec359fd46549db
Kazumasa Utashiro <kaz@utashiro.com>
2025-07-01 09:31:19 +0900

    add ex/dest.sh

---
+++ ex/dest.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+. "$(dirname $0)"/../getoptlong.sh
+
+set -eu
+
+declare -A OPTS=(
+    [ count     | c :COUNT=i # repeat count              ]=1
+    [ sleep     | i @SLEEP=f # interval time             ]=
+    [ paragraph | p ?PARA    # print newline after cycle ]=
+    [ trace     | x !TRACE   # trace execution           ]=
+    [ debug     | d +DEBUG   # debug level               ]=0
+    [ message   | m %MSG=(^(BEGIN|END|EACH)=) # print message at BEGIN|END|EACH ]=
+)
+TRACE() { [[ $2 ]] && set -x || set +x ; }
+
+getoptlong init OPTS
+getoptlong parse "$@" && eval "$(getoptlong set)"
+
+column=$(command -v column) || column=cat
+(( DEBUG >= 3 )) && dumpopt=(--all) filter=$column
+(( DEBUG >= 2 )) && getoptlong dump ${dumpopt[@]} | ${filter:-cat} >&2
+
+[[ ${1:-} =~ ^[0-9]+$ ]] && COUNT=$1 && shift
+
+message() { [[ -v MSG[$1] ]] && echo "${MSG[$1]}" || : ; }
+
+message BEGIN
+for (( i = 0; $# > 0 && i < COUNT ; i++ )) ; do
+    message EACH
+    (( DEBUG > 0 )) && echo "# [ ${@@Q} ]" >&2
+    "$@"
+    if (( COUNT > 0 )) ; then
+	[[ -v PARA ]] && echo "$PARA"
+	if (( ${#SLEEP[@]} > 0 )) ; then
+	    time="${SLEEP[$(( i % ${#SLEEP[@]} ))]}"
+	    (( DEBUG > 0 )) && echo "# sleep $time" >&2
+	    sleep $time
+	fi
+    fi
+done
+message END
